a850fc017ae1f4abc0987e7d1c89a0d9f4cdec6b
nullises <texanico@gmail.com>
2024-05-15 13:49:38 -0400

    fix/remove-unnecesary

--- src/utils/escatologico/Step1.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export function Step1(props) {
-  return (
-    <div className="flex justify-center items-center">
-      <p>ESTE ES UN TEXTO DE PRUEBA Y EJEMPLO</p>
-    </div>
-  );
-}
